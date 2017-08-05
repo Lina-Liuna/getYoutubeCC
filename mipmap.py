7e7f829eff172dc4466b9af7168d999a4cc34f3f
Lina Liu <Lina@Linas-MacBook-Pro.local>
2017-08-04 18:37:58 -0700

    first commit

---
+++ mipmap.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+def mipmap(filename):
+    with open(filename+".bak", "w") as targetf:
+        lines = []
+        with open(filename) as sourcef:
+            for line in sourcef:
+                line = line.strip()
+                if not line:
+                    continue
+
+                if re.search(r"^\d+", line):
+                    continue
+
+                line, _ = re.subn(r"<.*?>", " ", line)
+                lines.append(line)
+        targetf.write("".join(lines))
+
+
+if __name__ == "__main__":
+    mipmap(sys.argv[1])
